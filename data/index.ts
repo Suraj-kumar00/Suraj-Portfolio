df7ad8e507f8d191a0e61d699f0033fcf388aca8
Suraj <suraj.ukumar.p@gmail.com>
2024-07-27 00:36:49 +0530

    The portfolio is complete.

--- data/index.ts
+++ data/index.ts
@@ -72,132 +72,69 @@ export const gridItems = [
 export const projects = [
   {
     id: 1,
-    title: "3D Solar System Planets to Explore",
-    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-    img: "/p1.svg",
+    title: "DevsDiscord",
+    des: "DevsDiscord is a clone of Discord with the feature of only developer community engagement.",
+    img: "DevsDiscord.webp",
     iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
     link: "/ui.earth.com",
   },
   {
     id: 2,
-    title: "Yoom - Video Conferencing App",
-    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
-    img: "/p2.svg",
+    title: "ProActive",
+    des: "As we know Procrastination is a prevalent psychology-based problem that can be solved by ProActive a web-based Productivity tool.",
+    img: "/ProActive.png",
     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
     link: "/ui.yoom.com",
-  },
-  {
-    id: 3,
-    title: "AI Image SaaS - Canva Application",
-    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-    img: "/p3.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-    link: "/ui.aiimg.com",
-  },
-  {
-    id: 4,
-    title: "Animated Apple Iphone 3D Website",
-    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-    img: "/p4.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-    link: "/ui.apple.com",
-  },
+  }
 ];
 
 export const testimonials = [
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
+      "I highly recommend Suraj for his exceptional work as a frontend developer at Befrsher. His coding skills, attention to detail, and commitment to delivering high-quality user experiences make him a valuable asset to any team. Suraj consistently goes above and beyond in his work, and his positive attitude and collaboration skills are truly commendable. He would be a great addition to any project or organization.",
+      name: "Anamika Sahu",
+    title: "Marketing Manager at Befrsher",
   },
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
-  },
-  {
-    quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
-  },
-  {
-    quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
-  },
-  {
-    quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
-  },
+      "Suraj Kumar completed the Linux Foundation Mentorship program, specifically focusing on Cloudforet Plugin Development 2023, under the support of the Linux Foundation. He actively engaged in the project. While He initially faced some challenges in problem-solving skills during mentoring, He quickly adapted and showed potential to complete mentoring tasks efficiently.Throughout the mentoring period, Suraj had a high level of enthusiasm and responsibility.Through his passion and achievements, He had proven himself worthy of building a successful programming career.Given Suraj’s solid foundation in programming skill, I am confident that He will achieve outstanding results in his career. Considering his capabilities and enthusiasm, I firmly believe that He will become a successful software developer in the future.",
+      name: "Choonho Son",
+    title: "VP at KT Cloud | Cloudforet, LinuxFoundation Project, TSC chair",
+  }
 ];
 
 export const companies = [
-  {
-    id: 1,
-    name: "cloudinary",
-    img: "/cloud.svg",
-    nameImg: "/cloudName.svg",
-  },
-  {
-    id: 2,
-    name: "appwrite",
-    img: "/app.svg",
-    nameImg: "/appName.svg",
-  },
-  {
-    id: 3,
-    name: "HOSTINGER",
-    img: "/host.svg",
-    nameImg: "/hostName.svg",
-  },
-  {
-    id: 4,
-    name: "stream",
-    img: "/s.svg",
-    nameImg: "/streamName.svg",
-  },
-  {
-    id: 5,
-    name: "docker.",
-    img: "/dock.svg",
-    nameImg: "/dockerName.svg",
-  },
+  
 ];
 
 export const workExperience = [
   {
     id: 1,
-    title: "Frontend Engineer Intern",
-    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    title: "Technical Writer",
+    desc: "At Hashnode, I write about Open-Source, DevOps, JavaScript, Java, Python, Go, and development, documenting my technical learning journey. My blogs, which attract over 100 readers on average, are highly beneficial for those who prefer learning through written content.",
     className: "md:col-span-2",
     thumbnail: "/exp1.svg",
   },
   {
     id: 2,
-    title: "Mobile App Dev - JSM Tech",
-    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    title: "LFX Mentee at Linux Foundation",
+    desc: "Learned and worked on CloudForet Plugin Development project at Linux Foundation mentorship program.",
     className: "md:col-span-2", // change to md:col-span-2
-    thumbnail: "/exp2.svg",
+    thumbnail: "/LFX.jpeg",
   },
   {
     id: 3,
-    title: "Freelance App Dev Project",
-    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    title: "Frontend Engineer Intern ",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
     className: "md:col-span-2", // change to md:col-span-2
-    thumbnail: "/exp3.svg",
+    thumbnail: "/Frontend-engineer.webp",
   },
   {
     id: 4,
-    title: "Lead Frontend Developer",
-    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    title: "Software Engineering Fellow at Headstarter",
+    desc: "Selected as a SWE Fellow in Headstarter fellowship program 2024.",
     className: "md:col-span-2",
-    thumbnail: "/exp4.svg",
+    thumbnail: "/headstarter-logo.jpg",
   },
 ];
 
@@ -205,13 +142,16 @@ export const socialMedia = [
   {
     id: 1,
     img: "/git.svg",
+    link: "https://github.com/Suraj-kumar00",
   },
   {
     id: 2,
     img: "/twit.svg",
+    link: "https://x.com/surajk_umar01"
   },
   {
     id: 3,
     img: "/link.svg",
+    link: "https://www.linkedin.com/in/surajkumar00/",
   },
 ];
